34745210b49dc1c8c4ef88a4c3df8184005334d7
bankzhy <3532553868@qq.com>
2024-01-24 13:31:14 +0800

    spt_style

--- pretrain/spt_args.py
+++ pretrain/spt_args.py
@@ -153,7 +153,7 @@ class RuntimeArguments:
     )
 
     eval_batch_size: int = field(
-        default=16,
+        default=4,
         metadata={'help': 'Batch size for evaluation on each device'}
     )
 
