8a776d8f7e9f6747b339dcec96efd13be2dcb533
bankzhy <3532553868@qq.com>
2024-01-27 14:50:09 +0800

    spt_style

--- downstream/summarization/spt_args.py
+++ downstream/summarization/spt_args.py
@@ -60,7 +60,7 @@ class RuntimeArguments:
     )
 
     resume_from_checkpoint: str = field(
-        default=r"output/checkpoints/summarization/checkpoint-21500",
+        default=r"output/checkpoints/summarization/checkpoint-22500",
         metadata={'help': "The dataset directory."}
     )
 
