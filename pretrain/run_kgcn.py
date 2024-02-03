55882ef83e5c34ea7666f258bd772b7c8091cdb8
bankzhy <3532553868@qq.com>
2024-02-04 07:43:37 +0800

    spt_style

--- pretrain/run_kgcn.py
+++ pretrain/run_kgcn.py
@@ -180,7 +180,7 @@ def pre_train(args,
                                                      learning_rate=args.learning_rate,
                                                      weight_decay=args.lr_decay_rate,
                                                      max_grad_norm=args.grad_clipping_norm,
-                                                     num_train_epochs=20,
+                                                     num_train_epochs=10,
                                                      lr_scheduler_type=SchedulerType.LINEAR,
                                                      warmup_steps=args.warmup_steps,
                                                      logging_dir=os.path.join(args.tensor_board_root, task),
@@ -219,9 +219,9 @@ def pre_train(args,
             logger.info(f'Start pre-training task: {task}')
             # model device
             logger.info('Device: {}'.format(next(model.parameters()).device))
-            mass_result = trainer.train()
+            # mass_result = trainer.train()
             # Recover from checkpoint
-            # mass_result = trainer.train(resume_from_checkpoint=args.resume_from_checkpoint)
+            mass_result = trainer.train(resume_from_checkpoint=args.resume_from_checkpoint)
             logger.info(f'Pre-training task {task} finished')
             trainer.save_model(os.path.join(args.model_root, task))
         elif task == enums.TASK_MASS:
